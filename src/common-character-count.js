2651aa3b96b6e70abdeb218e9a53ae02bc81cc48
Viktar Brutsis <viktarbrutsis@gmail.com>
2023-04-13 21:51:20 +0300

    feat: draft for common chancter

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,8 +12,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  //строку в массив = > проверить каждый эелемент, если он есть то счетчик + и splice в обоих массивах
 }
 
 module.exports = {
